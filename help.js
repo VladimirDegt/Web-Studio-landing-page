8e8b93a75aa3c1317d4f8b20e226beaaab7504b4
Vladimir <107486166+VladimirDegt@users.noreply.github.com>
2023-03-08 11:10:54 +0200

    домашка 7 модальное окно

---
+++ help.js
@@ -0,0 +1,38 @@
+//! modern-normalize
+https://github.com/sindresorhus/modern-normalize
+https://cdnjs.com/libraries/modern-normalize
+
+//! Для генерації SVG-спрайту використовуй сервіс Icomoon.
+https://icomoon.io/
+
+//! Для оптимізації створеного SVG-спрайту використовуй сервіс svgomg.
+https://jakearchibald.github.io/svgomg/
+
+//? паттерн скрытия элемента
+.visually-hidden {
+    position: absolute;
+    white-space: nowrap;
+    width: 1px;
+    height: 1px;
+    overflow: hidden;
+    border: 0;
+    padding: 0;
+    clip: rect(0 0 0 0);
+    clip-path: inset(50%);
+    margin: -1px;
+}
+
+//? паттерн фиксации панели навигации
+<header class="scroll"></header>
+div.header {
+    display: flex;
+}
+const { height: pageHeaderHeight } = document
+.querySelector('.scroll')
+.getBoundingClientRect();
+
+document.body.style.paddingTop = `${pageHeaderHeight}px`;
+
+
+//? сбросить все свойства от браузера
+appearance: none;
